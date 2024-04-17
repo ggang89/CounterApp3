a867247e75f70ce0dc193771b0db3740fac1500c
ggang89 <ggang89@gmail.com>
2024-04-17 14:00:45 +0900

    useState연결해서 기본구조 완성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handlePlus = () => {
+    setCount(count+1);
+  };
+  const HandleMinus = () => {
+    setCount(count-1);
+  };
   return (
-    <div >
-      
+    <div>
+      <h1>Counter App 3</h1>
+      <div>
+        <button onClick={handlePlus}>+1</button>
+        <span>{count}</span>
+        <button onClick={HandleMinus}>-1</button>
+      </div>
     </div>
   );
 }
